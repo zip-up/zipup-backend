7e119a2e0dc99cf9d45157f71bb39953eddd8925
zwundzwzig <amoeba8282@gmail.com>
2024-03-25 01:11:53 +0900

    [chore] #8 ncp 연동

---
+++ src/test/java/com/zipup/server/global/config/JpaConfigTest.java
@@ -0,0 +1,48 @@
+package com.zipup.server.global.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class JpaConfigTest {
+
+    @Autowired
+    private DataSource dataSource;
+    @Mock
+    private EntityManager entityManager;
+    @InjectMocks
+    private JpaConfig jpaConfig;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        jpaConfig = new JpaConfig(entityManager);
+    }
+
+    @Test
+    public void testJpaDBConnection() {
+        try (Connection connection = dataSource.getConnection()) {
+            assertThat(connection).isNotNull();
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+    }
+
+    @Test
+    void testJpaConfig() {
+        assertNotNull(jpaConfig);
+    }
+}
---
+++ src/test/java/com/zipup/server/global/config/RedisConfigTest.java
@@ -0,0 +1,51 @@
+package com.zipup.server.global.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(properties = "spring.cloud.config.enabled=false")
+@Transactional
+class RedisConfigTest {
+
+  @Autowired
+  private LettuceConnectionFactory lettuceConnectionFactory;
+  @Autowired
+  private RedisTemplate<String, String> redisTemplate;
+
+  @Test
+  @DisplayName("redis 연결 실패 테스트")
+  public void testRedisDisConnection() {
+    redisTemplate.setConnectionFactory(new LettuceConnectionFactory("localhost", 1111));
+
+    assertThrows(IllegalStateException.class, ()
+            -> redisTemplate.opsForValue().get("key"));
+  }
+
+  @Test
+  @DisplayName("redis 연결 테스트")
+  public void testRedisConnection() {
+    RedisConnection connection = lettuceConnectionFactory.getConnection();
+    assertNotNull(connection);
+    connection.close();
+  }
+
+  @Test
+  @DisplayName("redis 삽입 후 조회")
+  public void testRedisTemplateUsage() {
+    redisTemplate.opsForValue().set("test-key", "test-value");
+
+    String result = redisTemplate.opsForValue().get("test-key");
+
+    assertEquals("test-value", result);
+    redisTemplate.delete("test-key");
+  }
+
+}
