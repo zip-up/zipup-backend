963d42b8e946b900193f3b2f1e91c8d12f632c57
zwundzwzig <amoeba8282@gmail.com>
2024-03-29 18:32:02 +0900

    [fix] #47 return accesstoken in body & refactor order id validate

--- src/main/java/com/zipup/server/user/dto/SignInResponse.java
+++ src/main/java/com/zipup/server/user/dto/SignInResponse.java
@@ -2,14 +2,15 @@ package com.zipup.server.user.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 
-@Getter
 @Builder
+@Data
 public class SignInResponse {
   @Schema(description = "유저 식별자")
   private String id;
   private String name;
   private String email;
   private String profileImage;
+  private String accesstoken;
 }
